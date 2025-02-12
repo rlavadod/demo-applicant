53fa85a2050324113faf26f7fc013d2e83c6df11
ruddylavado <rlavadod@gmail.com>
2025-02-11 22:22:57 -0500

    feat: add openapi documentation

--- src/main/java/com/example/demoapplicant/controller/ApplicantController.java
+++ src/main/java/com/example/demoapplicant/controller/ApplicantController.java
@@ -2,6 +2,13 @@ package com.example.demoapplicant.controller;
 
 import com.example.demoapplicant.model.api.Applicant;
 import com.example.demoapplicant.service.ApplicantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,14 +29,33 @@ public class ApplicantController {
   @Autowired
   private ApplicantService service;
 
+  @Operation(summary = "Create a new applicant")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201",
+          description = "Applicant created successfully",
+          content = {@Content(mediaType = "application/json",
+              schema = @Schema(implementation = Applicant.class))}),
+      @ApiResponse(responseCode = "400", description = "Invalid input provided")})
   @PostMapping(value = "/")
-  public ResponseEntity<?> saveApplicant(@RequestBody Applicant applicant) {
+  public ResponseEntity<?> saveApplicant(@io.swagger.v3.oas.annotations.parameters.RequestBody(
+      description = "Applicant to create",
+      required = true,
+      content = @Content(mediaType = "application/json",
+          schema = @Schema(implementation = Applicant.class),
+          examples = @ExampleObject(value = "{\"name\":\"Carlos\",\"lastname\":\"Alcantara\",\"birthdate\":\"1980-02-09\"}")))
+                                         @RequestBody Applicant applicant) {
     return new ResponseEntity<>(service.saveApplicant(applicant), HttpStatus.CREATED);
   }
 
+  @Operation(summary = "Obtain all applicants")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Obtain all applicants",
+          content = {@Content(mediaType = "application/json",
+              array = @ArraySchema(schema = @Schema(implementation = Applicant.class)))}),
+      @ApiResponse(responseCode = "400", description = "Not found any applicant",
+          content = @Content)})
   @GetMapping("/")
-  public ResponseEntity<List<Applicant>> getAllUsers() {
+  public ResponseEntity<List<Applicant>> getAllApplicants() {
     return ResponseEntity.ok(service.getAll());
   }
-
 }
--- src/main/java/com/example/demoapplicant/controller/MetricsController.java
+++ src/main/java/com/example/demoapplicant/controller/MetricsController.java
@@ -2,6 +2,11 @@ package com.example.demoapplicant.controller;
 
 import com.example.demoapplicant.model.api.Metrics;
 import com.example.demoapplicant.service.MetricsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +22,21 @@ public class MetricsController {
   @Autowired
   private MetricsService service;
 
+  @Operation(summary = "Obtain all age average")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Obtain all average applicant age",
+          content = {@Content(mediaType = "application/json",
+              schema = @Schema(implementation = Metrics.class))})})
   @GetMapping("/age-average")
   public ResponseEntity<Metrics> getAllAgeAverage() {
     return ResponseEntity.ok(service.obtainAllAverageAge());
   }
 
+  @Operation(summary = "Obtain all age standard deviation")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Obtain all age standard deviation applicants",
+          content = {@Content(mediaType = "application/json",
+              schema = @Schema(implementation = Metrics.class))})})
   @GetMapping("/age-standard-deviation")
   public ResponseEntity<Metrics> getAllAgeStandardDeviation() {
     return ResponseEntity.ok(service.obtainAllStandardDeviation());
